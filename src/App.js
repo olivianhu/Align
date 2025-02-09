ff5b5b41a59d8e91f8b62e19000f4435f05a056f
Olivia Hu <oliviahu2020@gmail.com>
2025-02-09 14:52:01 -0500

    Adds login and creation page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
 import './App.css';
 import { useState } from 'react'
 import Axios from 'axios'
+import Login from './components/Login';
 
 export default function App() {
 
     function sendMessage() {
-        Axios.get('http://localhost:5000/')
-        .then(function (response) {
-            console.log('response successfully received, response below')
-            console.log(response)
+      Axios.get('http://localhost:5000/')
+      .then(function (response) {
+        console.log('response successfully received, response below')
+        console.log(response)
 
-            //set message to be response we get from backend
-            setMessage(response.data);
-        }).catch(function (error) {
-            console.log('response unsuccessfully received, error below')
-            console.log(error)
-        }).finally(function (){
-            console.log("This part is always executed no matter what")
-        }) 
-
+        setMessage(response.data);
+      }).catch(function (error) {
+          console.log('response unsuccessfully received, error below')
+          console.log(error)
+      })
     }
 
     const [message, setMessage] = useState("No Current Message")
     
     return (
         <div className='container'>
-            <p>{message}</p>
+            {/* <p>{message}</p>
             <button
             onClick={() => {sendMessage()}}
-            >Send Message</button>
+            >Send Message</button> */}
+            
+            <Login />
         </div>
     )
 }
