7ee0599dbdafe04a2c0f037cb35ceedeec0fce83
Olivia Hu <oliviahu2020@gmail.com>
2025-02-11 17:15:37 -0500

    implements basic css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
 import './App.css';
 import { useState } from 'react'
-import Axios from 'axios'
+import Axios from 'axios';
 import Login from './components/Login';
 import CreationPage from './components/CreationPage';
 
 export default function App() {
 
-    function sendMessage() {
-      Axios.get('http://localhost:5000/')
-      .then(function (response) {
-        console.log('response successfully received, response below')
-        console.log(response)
+    // function sendMessage() {
+    //   Axios.get('http://localhost:5000/')
+    //   .then(function (response) {
+    //     console.log('response successfully received, response below')
+    //     console.log(response)
 
-        setMessage(response.data);
-      }).catch(function (error) {
-          console.log('response unsuccessfully received, error below')
-          console.log(error)
-      })
-    }
+    //     setMessage(response.data);
+    //   }).catch(function (error) {
+    //       console.log('response unsuccessfully received, error below')
+    //       console.log(error)
+    //   })
+    // }
 
-    const [message, setMessage] = useState("No Current Message")
+    // const [message, setMessage] = useState("No Current Message")
     const [showLogin, setShowLogin] = useState(false)
     
     return (
-        <div className='container'>
+        <div className='p-10'>
             {showLogin ? <Login /> : <CreationPage />}
-            <p>{message}</p>
+            {/* <p>{message}</p>
             <button
             onClick={() => {sendMessage()}}
-            >Send Message</button>
+            >Send Message</button> */}
         </div>
     )
 }
--- src/components/CreationPage.jsx
+++ src/components/CreationPage.jsx
@@ -2,77 +2,99 @@
 const CreationPage = () => {
   return(
     <div>
-      <h1>Meeting Creation</h1>
-      <form>
-        <label>Name: </label>
-        <input
-            type="text"
-            placeholder="Name"
-          />
-        <br />
-        <label for="times">What times?</label>
-        <select name="start-time" id="start-time">
-          <option value="0">12 a.m.</option>
-          <option value="1">1 a.m.</option>
-          <option value="2">2 a.m.</option>
-          <option value="3">3 a.m.</option>
-          <option value="4">4 a.m.</option>
-          <option value="5">5 a.m.</option>
-          <option value="6">6 a.m.</option>
-          <option value="7">7 a.m.</option>
-          <option value="8">8 a.m.</option>
-          <option value="9">9 a.m.</option>
-          <option value="10">10 a.m.</option>
-          <option value="11">11 a.m.</option>
-          <option value="12">12 p.m.</option>
-          <option value="13">1 p.m.</option>
-          <option value="14">2 p.m.</option>
-          <option value="15">3 p.m.</option>
-          <option value="16">4 p.m.</option>
-          <option value="17">5 p.m.</option>
-          <option value="18">6 p.m.</option>
-          <option value="19">7 p.m.</option>
-          <option value="20">8 p.m.</option>
-          <option value="21">9 p.m.</option>
-          <option value="22">10 p.m.</option>
-          <option value="23">11 p.m.</option>
-        </select>
-        to
-        <select name="end-time" id="end-time">
-          <option value="0">12 a.m.</option>
-          <option value="1">1 a.m.</option>
-          <option value="2">2 a.m.</option>
-          <option value="3">3 a.m.</option>
-          <option value="4">4 a.m.</option>
-          <option value="5">5 a.m.</option>
-          <option value="6">6 a.m.</option>
-          <option value="7">7 a.m.</option>
-          <option value="8">8 a.m.</option>
-          <option value="9">9 a.m.</option>
-          <option value="10">10 a.m.</option>
-          <option value="11">11 a.m.</option>
-          <option value="12">12 p.m.</option>
-          <option value="13">1 p.m.</option>
-          <option value="14">2 p.m.</option>
-          <option value="15">3 p.m.</option>
-          <option value="16">4 p.m.</option>
-          <option value="17">5 p.m.</option>
-          <option value="18">6 p.m.</option>
-          <option value="19">7 p.m.</option>
-          <option value="20">8 p.m.</option>
-          <option value="21">9 p.m.</option>
-          <option value="22">10 p.m.</option>
-          <option value="23">11 p.m.</option>
-        </select>
-        <br />
-        <label for="dates">What dates?</label>
-        <input
-            type="date"
-          />
-        to
-        <input
-            type="date"
-          />
+      <h1 className="text-xl font-bold mb-4">Meeting Creation</h1>
+      <form className="flex flex-col gap-3">
+        
+        <div>
+          <label className="text-lg">Name: </label>
+          <input
+              type="text"
+              placeholder="Name"
+              className="border border-gray-300 rounded-md text-sm px-1"
+            />
+        </div>
+
+        <div>
+          <label htmlFor="times">What times?</label>
+          <div className="flex gap-2">
+            <select 
+              name="start-time" 
+              id="start-time"
+              className="border border-gray-300 rounded-md"
+            >
+              <option value="0">12 a.m.</option>
+              <option value="1">1 a.m.</option>
+              <option value="2">2 a.m.</option>
+              <option value="3">3 a.m.</option>
+              <option value="4">4 a.m.</option>
+              <option value="5">5 a.m.</option>
+              <option value="6">6 a.m.</option>
+              <option value="7">7 a.m.</option>
+              <option value="8">8 a.m.</option>
+              <option value="9">9 a.m.</option>
+              <option value="10">10 a.m.</option>
+              <option value="11">11 a.m.</option>
+              <option value="12">12 p.m.</option>
+              <option value="13">1 p.m.</option>
+              <option value="14">2 p.m.</option>
+              <option value="15">3 p.m.</option>
+              <option value="16">4 p.m.</option>
+              <option value="17">5 p.m.</option>
+              <option value="18">6 p.m.</option>
+              <option value="19">7 p.m.</option>
+              <option value="20">8 p.m.</option>
+              <option value="21">9 p.m.</option>
+              <option value="22">10 p.m.</option>
+              <option value="23">11 p.m.</option>
+            </select>
+            to
+            <select 
+              name="end-time" 
+              id="end-time"
+              className="border border-gray-300 rounded-md"
+            >
+              <option value="0">12 a.m.</option>
+              <option value="1">1 a.m.</option>
+              <option value="2">2 a.m.</option>
+              <option value="3">3 a.m.</option>
+              <option value="4">4 a.m.</option>
+              <option value="5">5 a.m.</option>
+              <option value="6">6 a.m.</option>
+              <option value="7">7 a.m.</option>
+              <option value="8">8 a.m.</option>
+              <option value="9">9 a.m.</option>
+              <option value="10">10 a.m.</option>
+              <option value="11">11 a.m.</option>
+              <option value="12">12 p.m.</option>
+              <option value="13">1 p.m.</option>
+              <option value="14">2 p.m.</option>
+              <option value="15">3 p.m.</option>
+              <option value="16">4 p.m.</option>
+              <option value="17">5 p.m.</option>
+              <option value="18">6 p.m.</option>
+              <option value="19">7 p.m.</option>
+              <option value="20">8 p.m.</option>
+              <option value="21">9 p.m.</option>
+              <option value="22">10 p.m.</option>
+              <option value="23">11 p.m.</option>
+            </select>
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="dates">What dates?</label>
+          <div className="flex gap-2">
+            <input
+                type="date"
+                className="border border-gray-300 rounded-md"
+              />
+            to
+            <input
+                type="date"
+                className="border border-gray-300 rounded-md"
+              />
+          </div>
+        </div>
       </form>
     </div>
   )
